e2965a10f537b7f1e2c7eecebe17833ca32b3b9f
Goyder <matthew.goyder@intel.com>
2017-08-22 20:54:04 -0700

    Added Azure working version of the socket test

--- socket_test/app.js
+++ socket_test/app.js
@@ -1,10 +1,17 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var port = process.env.PORT || 3000;
+
+server.listen(port, function () {  
+	var addr = server.address();
+	console.log('   app listening on http://' + addr.address + ':' + addr.port);
+  });
 
 app.get('/', function(req, res)
 {
-	// res.send('<h1>hello world</h1>');
+//	 res.send('<h1>hello world</h1>');
 	res.sendFile(__dirname + '/index.html');
 });
 
@@ -34,8 +41,4 @@ io.on('connection', function(socket)
 		
 		fs.writeFile('file', buf);
 	});
-});
-
-http.listen(7777, function(){
-	console.log('listening on *:7777');
 });
